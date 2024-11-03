90d347a97fe9c87a3aba3e0bbc309ab6c488d17c
Chris Gurrin <cgurrindev@gmail.com>
2024-11-03 15:55:19 +1000

    make list view rounded

--- src/components/sidebar/item-list/item-list.tsx
+++ src/components/sidebar/item-list/item-list.tsx
@@ -7,19 +7,21 @@ type Props = {
 
 export const ItemList = ({ items }: Props) => {
     return (
-        <div className="flex flex-col flex-grow gap-0.5 overflow-y-scroll bg-zinc-900 p-0.5">
-            {items.map((item) => {
-                return <Item key={item.item} item={item} />;
-            })}
-            <div
-                className={clsx(
-                    'w-full h-full flex justify-center items-center',
-                    {
-                        hidden: items.length > 0,
-                    },
-                )}
-            >
-                No items.
+        <div className="rounded-md flex-grow overflow-hidden">
+            <div className="h-full flex flex-col gap-0.5 overflow-y-scroll bg-zinc-900 p-0.5">
+                {items.map((item) => {
+                    return <Item key={item.item} item={item} />;
+                })}
+                <div
+                    className={clsx(
+                        'w-full h-full flex justify-center items-center',
+                        {
+                            hidden: items.length > 0,
+                        },
+                    )}
+                >
+                    No items.
+                </div>
             </div>
         </div>
     );
